9e74043e4ab45474f79cd8eb08d51d57ed5371c0
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2023-11-06 16:06:53 +0200

    toimiva versio

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Account, Category, Document, Transaction
 
 # Register your models here.
+
+@admin.register(Document)
+class AccountAdmin(ModelAdmin):
+    pass
+
+@admin.register(Category)
+class AccountAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Account)
+class AccountAdmin(ModelAdmin):
+    pass
+
+@admin.register(Transaction)
+class AccountAdmin(ModelAdmin):
+    list_display = [
+        "id",
+        "account",
+        "date",
+        "amount",
+        "type",
+        "state",
+        "category",
+        "comment",
+    ]
+    list_display_links = list_display
--- finance/models.py
+++ finance/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class TimestampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("luotu"),)
+
 
     class Meta:
         abstract = True
@@ -11,7 +12,8 @@ class TimestampModel(models.Model):
 class OwnedModel(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name=_("omistaja"),
     )
 
     class Meta:
@@ -24,22 +26,45 @@ class Document(TimestampModel, OwnedModel):
         CALCULATION = ("CALCULATION", _("Laskelma"))
         OTHER = ("OTHER", _("Muu"))
 
-    type = models.CharField(max_length=20, choices=Type.choices)
-    file = models.FileField(upload_to="docs/%Y-%m/")
+    type = models.CharField(max_length=20, choices=Type.choices, verbose_name=_("tyyppi"))
+    name = models.CharField(max_length=100, blank=True, verbose_name=_("nimi"))
+    file = models.FileField(upload_to="docs/%Y-%m/", verbose_name=_("tiedosto"))
+
+    class Meta:
+        verbose_name = _("dokumentti")
+        verbose_name_plural = _("dokumentit")
+
+    def __str__(self):
+        return self.name if self.name else f"Document {self.id}"
    
 class Category(TimestampModel, OwnedModel):
-    name: models.CharField(max_length=100)
+    name: models.CharField(max_length=100, verbose_name=_("nimi"),)
     parent = models.ForeignKey(
         "self",
         blank=True,
         null=True,
         related_name="children",
         on_delete=models.CASCADE,
+        verbose_name=_("yläkategoria"),
     )
+
+    class Meta:
+        verbose_name = _("kategoria")
+        verbose_name_plural = _("kategoriat")
+
+    def __str__(self):
+        prefix = f"{self.parent} / " if self.parent else ""
+        return f"{prefix}{self.name}"
    
 class Account(TimestampModel, OwnedModel):
-    name = models.CharField(max_length=100)
-    bank_account = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name=_("nimi"),)
+    bank_account = models.CharField(max_length=50, null=True, blank=True, verbose_name=_("pankkitili"),)
+
+    class Meta:
+        verbose_name = _("tili")
+        verbose_name_plural = _("tilit")
+    def __str__(self):
+        return f"{self.id:04d} {self.name}"
 
 
 class Transaction(TimestampModel):
@@ -51,12 +76,33 @@ class Transaction(TimestampModel):
         UPCOMING = ("UPCOMING", _("Tuleva"))
         DONE = ("DONE", _("Tapahtumat"))
 
-    account = models.ForeignKey(Account, on_delete=models.RESTRICT)
-    type = models.CharField(max_length=20, choices=Type.choices)
-    state = models.CharField(max_length=20, choices=State.choices)
-    date = models.DateField()
-    Category = models.ForeignKey(
-        Category, null=True, blank=True, on_delete=models.SET_NULL
+    account = models.ForeignKey(Account, on_delete=models.RESTRICT, verbose_name=_("tili"),)
+    type = models.CharField(max_length=20, choices=Type.choices, verbose_name=_("tyyppi"),)
+    state = models.CharField(max_length=20, choices=State.choices, verbose_name=_("tila"),)
+    date = models.DateField(verbose_name=_("päiväys"),)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, verbose_name=_("määrä"),)    
+    comment = models.CharField(max_length=200, blank=True, verbose_name=_("kommentti")) 
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_("kategoria"),
     )
-    documents = models.ManyToManyField(Document, related_name="transactions", blank=True,)
+    documents = models.ManyToManyField(Document, related_name="transactions", blank=True, verbose_name=_("dokumentit"),)
+    
+    class Meta:
+        verbose_name = ("tilitapahtuma")
+        verbose_name_plural = _("tilitapahtumat")
+
+    def __str__(self):
+        return (
+            f"{self.date} {self.amount:8.2f} "
+            f"{self.comment} ({self.State(self.State).label})"
+        )
+    
+
+# lukutyypit:
+#   int = konkonaisluku, esim. 1,5,7, 11 tai 0, -3
+#   float = liukuluku, esim. 1.0, 2.6, 3.83, -0.8
+#   Decimal = desimaaliluku, esim. Decimal('0.1'), Decimal('-11.8')
+# from decimal import Decimal
+
+
 
